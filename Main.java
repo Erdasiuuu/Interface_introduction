9e02d9a39443f37e05e8ba5e0aed23b6ca1c7a56
Egor <elin4cod@gmail.com>
2024-05-23 03:29:04 +0700

    main greeting

--- Main.java
+++ Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String... args) {
 	 Scanner scanner = new Scanner(System.in);
+	 System.out.printf("Введите первое число");
 	int n1 = scanner.nextInt();
+	scanner.nextLine();
+	 System.out.printf("Введите второе число");
 	int n2 = scanner.nextInt();
+	scanner.nextLine();
 	Matcher matcher;
 	matcher = new SimpleMatcher();
-        System.out.println(matcher.match(n1, n2));
+        System.out.printf("Первое число больше второго: %d", matcher.match(n1, n2));
 	matcher = new PrimeMatcher();
-        System.out.print(matcher.match(n1, n2));
+        System.out.printf("Числа взаимно простые: %d", matcher.match(n1, n2));
     }
 }
